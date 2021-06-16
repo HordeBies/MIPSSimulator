221677b9ad0f29348d54c2800f6d529d4a0c093c
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2021-06-16 15:39:50 +0300

    instructions main init

--- Sim/Instructions.cs
+++ Sim/Instructions.cs
@@ -9,506 +9,186 @@ namespace MIPS.Sim
 	public partial class MIPSSimulator
 	{
 		private void OpError()
-        {
+		{
 			gui.ReportError("Error: invalid usage of instruction");
 			return;
 		}
-		private bool FindRegister(string id)
+		private short ConvertSignedNumber(string number)
+        {
+			short result;
+			if(number[0] == '1')
+            {
+				result = (short)(0 - Convert.ToInt16(number.Substring(1), 2));
+            }
+            else
+            {
+				result = Convert.ToInt16(number.Substring(1), 2);
+			}
+			return result;
+        }
+		private Register FetchRd(string instruction)
 		{
-			return true;
-			//bool foundRegister = false;
-			//if (CurrentInstruction.ElementAt(0) != '$' || CurrentInstruction.Length < 2)
-			//{
-			//    gui.ReportError("Error: Register expected");
-			//    return foundRegister;
-			//}
-			//CurrentInstruction = CurrentInstruction.Substring(1);
-			//string register = CurrentInstruction.Substring(0, 2);
-			//if (register == "ze" && CurrentInstruction.Length >= 4)
-			//{
-			//    register += CurrentInstruction.Substring(2, 2);
-			//}
-			//else if (register == "ze")
-			//{
-			//    gui.ReportError("Error: Register expected");
-			//    return foundRegister;
-			//}
-			//register = "$" + register;
-			//for (int i = 0; i < Registers.Count; i++)
-			//{
-			//    if (register == Registers[i].Label)
-			//    {
-			//        args[num] = i;
-			//        foundRegister = true;
-			//        if (i != 0)
-			//        {
-			//            CurrentInstruction = CurrentInstruction.Substring(2);
-			//        }
-			//        else
-			//        {
-			//            CurrentInstruction = CurrentInstruction.Substring(4);
-			//        }
-			//    }
-			//}
-			//if (!foundRegister)
-			//{
-			//    gui.ReportError("Error: Invalid register");
-			//}
-			//return foundRegister;
+			return Registers[Convert.ToInt16(instruction.Substring(10, 3), 2)];
 		}
-		//void add()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) + int.Parse(Registers[args[1]].Value)))
-		//			return;
-		//	} 
-		//	if(args[0] != 0 && args[0] != 1 &&  args[1] != 1 && args[2] != 1)
-		//          {
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) + int.Parse(Registers[args[2]].Value)).ToString();
-		//          }
-		//          else
-		//          {
-		//		OpError();
-		//          }
-		//}
-		//void addi()
-		//{
-		//	bool spFlag = false;
-		//	if(args[0] == 29)
-		//          {
-		//		spFlag = true;
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) + args[2]*4))
-		//			return;
-		//          }
-		//	if(args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//          {
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) + (spFlag?args[2]*4:args[2])).ToString();
-		//          }
-		//          else
-		//          {
-		//		OpError();
-		//		return;
-		//          }
-		//}
-		//void sub()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) - int.Parse(Registers[args[1]].Value)))
-		//			return;
-		//	}
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1 && args[2] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) - int.Parse(Registers[args[2]].Value)).ToString();
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
-		//void and()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) & int.Parse(Registers[args[1]].Value)))
-		//			return;
-		//	}
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1 && args[2] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) & int.Parse(Registers[args[2]].Value)).ToString();
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
-		//void andi()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) & args[2]))
-		//			return;
-		//	}
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) & args[2]).ToString();
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
 
-		//}
-		//void or()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) | int.Parse(Registers[args[1]].Value)))
-		//			return;
-		//	}
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1 && args[2] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) | int.Parse(Registers[args[2]].Value)).ToString();
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
-		//void ori()
-		//{
-		//	if (args[0] == 29)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) | args[2]))
-		//			return;
-		//	}
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) | args[2]).ToString();
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void slt()
-		//{
-		//	if(args[0] != 0 && args[0] != 1 && args[1] != 1 && args[2] != 1)
-		//          {
-		//		Registers[args[0]].Value =(int.Parse(Registers[args[1]].Value) < int.Parse(Registers[args[2]].Value)) ? "1":"0";
-		//          }
-		//          else
-		//          {
-		//		OpError();
-		//		return;
-		//          }
-		//}
-		//void slti()
-		//{
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//	{
-		//		Registers[args[0]].Value = (int.Parse(Registers[args[1]].Value) < args[2]) ? "1" : "0";
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void lw()
-		//{
-		//	if(args[0] == 29)
-		//          {
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value + args[2]*4)))
-		//			return;
-		//          }
-		//	if(args[0] != 0 && args[0] != 1 && args[2] == -1)
-		//          {
-		//		Registers[args[0]].Value = MemoryTable[args[1]].Value;
-		//          }
-		//	else if(args[0] != 0 && args[0] != 1) 
-		//	{
-		//		int offsetPos = ((int.Parse(Registers[args[1]].Value) - 40000) / 4 + args[2] + 1);
-		//		if (offsetPos > 99 || offsetPos < 0)
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		Registers[args[0]].Value = Stack[offsetPos].Value;
-		//	}
-		//          else
-		//          {
-		//		OpError();
-		//		return;
-		//          }
-		//}
-		//      void sw()
-		//{
-		//	if(args[0] != 1 && args[2] == -1)
-		//          {
-		//		MemoryTable[args[1]].Value = Registers[args[0]].Value.ToString();
-		//          }else if(args[0] != 1)
-		//          {
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) + args[2] * 4))
-		//              {
-		//			OpError();
-		//			return;
-		//              }
-		//		int offsetPos = ((int.Parse(Registers[args[1]].Value) -40000)/4 + args[2] +1);
-		//		if (offsetPos > 99 || offsetPos < 0)
-		//              {
-		//			OpError();
-		//			return;
-		//              }
-		//		Stack[offsetPos].Value = Registers[args[0]].Value;
-		//          }
-		//          else
-		//          {
-		//		OpError();
-		//		return;
-		//          }
-		//}
-		//void beq()
-		//{
-		//	if (args[0] != 1 && args[1] != 1)
-		//	{
-		//		LastLine = CurrentLine;
-		//		if (int.Parse(Registers[args[0]].Value) == int.Parse(Registers[args[1]].Value))
-		//		{
-		//			CurrentLine = args[2];
-		//		}
-		//		else
-		//			CurrentLine++;
-		//          }
-		//          else
-		//          {
-		//		OpError();
-		//          }
-		//}
-		//void blt()
-		//{
-		//	if(args[0] != 1 && args[1] != 1)
-		//          {
-		//		LastLine = CurrentLine;
-		//		if ((int.Parse((Registers[args[0]].Value))) < (int.Parse(Registers[args[1]].Value)))
-		//		{
-		//			CurrentLine = args[2];
-		//		}
-		//		else
-		//			CurrentLine++;
-		//          }
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
-		//void j()
-		//{
-		//	LastLine = CurrentLine;
-		//	CurrentLine = args[0];
-		//}
-		//void ldc1()
-		//{
-		//	if(args[0] > 31 && args[2] == -1)
-		//          {
-		//              if (!MemoryTable[args[1]].floatFlag)
-		//              {
-		//			OpError();
-		//			return;
-		//              }
-		//		Registers[args[0]].Value = MemoryTable[args[1]].Value;
-		//		Registers[args[0]+1].Value = MemoryTable[args[1]].Value;
-		//	}
-		//	else if (args[0] > 31)
-		//          {
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) + args[2]*4))
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		int offsetPos = ((int.Parse(Registers[args[1]].Value) - 40000) / 4 + args[2] + 1);
-		//		if (offsetPos > 99 || offsetPos < 0)
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		if(Stack[offsetPos].Value == Stack[offsetPos - 1].Value)
-		//              {
-		//			Registers[args[0]].Value = Stack[offsetPos].Value;
-		//			Registers[args[0]+1].Value = Stack[offsetPos].Value;
-		//		}
-		//		else
-		//              {
-		//                  OpError();
-		//              }
+		private Register FetchRt(string instruction)
+		{
+			return Registers[Convert.ToInt16(instruction.Substring(7, 3),2)];
+		}
 
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void sdc1()
-		//{
-		//	if (args[0] > 31 && args[2] == -1)
-		//          {
-		//		if (!MemoryTable[args[1]].floatFlag)
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		MemoryTable[args[1]].Value = Registers[args[0]].Value ;
-		//	}
-		//	else if (args[0] > 31)
-		//	{
-		//		if (!checkStackBounds(int.Parse(Registers[args[1]].Value) + args[2]*4))
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		int offsetPos = ((int.Parse(Registers[args[1]].Value) - 40000) / 4 + args[2] + 1);
-		//		if (offsetPos > 99 || offsetPos < 0 || offsetPos-1 <0 || offsetPos -1 > 99)
-		//		{
-		//			OpError();
-		//			return;
-		//		}
-		//		Stack[offsetPos].Value = Registers[args[0]].Value;
-		//		Stack[offsetPos-1].Value = Registers[args[0]].Value;
-		//	}
-		//}
-		//void addd()
-		//{
-		//          if (args[0] > 31 && args[1] > 31 && args[2] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0 && args[2] % 2 == 0)
-		//          {
-		//		string res = (double.Parse(Registers[args[1]].Value) + double.Parse(Registers[args[2]].Value)).ToString();
-		//		Registers[args[0]].Value = res;
-		//		Registers[args[0]+1].Value = res;
-		//	}
-		//	else
-		//          {
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void subd()
-		//{
+		private Register FetchRs(string instruction)
+		{
+			return Registers[Convert.ToInt16(instruction.Substring(4, 3),2)];
+		}
+		private short FetchOffset(string instruction)
+		{
+			return ConvertSignedNumber(instruction.Substring(10, 6));
+		}
 
-		//	if (args[0] > 31 && args[1] > 31 && args[2] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0 && args[2] % 2 == 0)
-		//	{
-		//		string res = (double.Parse(Registers[args[1]].Value) - double.Parse(Registers[args[2]].Value)).ToString();
-		//		Registers[args[0]].Value = res;
-		//		Registers[args[0]+1].Value = res;
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
+		private short FetchJumpOffset(string instruction)
+		{
+			return ConvertSignedNumber(instruction.Substring(4, 12));
+		}
 
-		//}
-		//void divd()
-		//{
-		//	if (args[0] > 31 && args[1] > 31 && args[2] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0 && args[2] % 2 == 0)
-		//	{
-		//		string res = (double.Parse(Registers[args[1]].Value) / double.Parse(Registers[args[2]].Value)).ToString();
-		//		Registers[args[0]].Value = res;
-		//		Registers[args[0] + 1].Value = res;
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void muld()
-		//{
-		//	if (args[0] > 31 && args[1] > 31 && args[2] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0 && args[2] % 2 == 0)
-		//	{
-		//		string res = (double.Parse(Registers[args[1]].Value) * double.Parse(Registers[args[2]].Value)).ToString();
-		//		Registers[args[0]].Value = res;
-		//		Registers[args[0] + 1].Value = res;
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void ceqd()
-		//{
-		//	if (args[0] > 31 && args[1] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0)
-		//	{
-		//              FPA = double.Parse(Registers[args[0]].Value).Equals(double.Parse(Registers[args[1]].Value));
-		//          }
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
+		void Add(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
 
-		//}
-		//void cltd()
-		//{
-		//	if (args[0] > 31 && args[1] > 31 && args[0] % 2 == 0 && args[1] % 2 == 0)
-		//	{
-		//		FPA = double.Parse(Registers[args[0]].Value) < double.Parse(Registers[args[1]].Value);
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//		return;
-		//	}
-		//}
-		//void bc1t()
-		//{
-		//	LastLine = CurrentLine;
-		//	if(FPA == true)
-		//          {
-		//		CurrentLine = args[0];
-		//          }
-		//          else
-		//          {
-		//		CurrentLine++;
-		//          }
-		//}
-		//void bc1f()
-		//{
-		//	LastLine = CurrentLine;
-		//	if (FPA == false)
-		//	{
-		//		CurrentLine = args[0];
-		//          }
-		//          else
-		//          {
-		//		CurrentLine++;
-		//          }
-		//}
-		//void mfhi()
-		//      {
-		//	if(args[0] < 31 && args[0] > 1)
-		//          {
-		//		Registers[args[0]].Value = MFHI.ToString();
-		//          }
-		//      }
-		//void mflo()
-		//{
-		//	if (args[0] < 31 && args[0] > 1)
-		//	{
-		//		Registers[args[0]].Value = MFLO.ToString();
-		//	}
-		//}
-		//void mult()
-		//{
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//	{
-		//		long a = int.Parse(Registers[args[0]].Value);
-		//              long b = int.Parse(Registers[args[1]].Value);
-		//		long c = a * b;
-		//		long hi = (c >> 32);
-		//		MFLO = (int)(c);
-		//		MFHI = (int)(hi);
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
-		//void div()
-		//{
-		//	if (args[0] != 0 && args[0] != 1 && args[1] != 1)
-		//	{
-		//		MFLO = (int.Parse(Registers[args[0]].Value) / int.Parse(Registers[args[1]].Value));
-		//		MFHI = (int.Parse(Registers[args[0]].Value) % int.Parse(Registers[args[1]].Value));
-		//	}
-		//	else
-		//	{
-		//		OpError();
-		//	}
-		//}
+			rd.Value = (short)(rt.Value + rs.Value);
+		}
+
+		void Sub(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+
+			rd.Value = (short)(rs.Value - rt.Value);
+		}
+
+		void And(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+
+			rd.Value = (short)(rs.Value & rt.Value);
+		}
+
+		void Or(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+
+			rd.Value = (short)(rs.Value | rt.Value);
+		}
+
+		void Slt(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rs = FetchRs(instruction);
+			Register rt = FetchRt(instruction);
+
+			rd.Value = (short)(rs.Value < rt.Value ? 1 : 0);
+		}
+
+		void Mul(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+			var rs1 = Convert.ToUInt16(rs.Binary.Substring(8, 8),2);
+			var rt1 = Convert.ToUInt16(rt.Binary.Substring(8, 8),2);
+			rd.Value = (short)(rs1 * rt1);
+		}
+
+		void Sll(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rs = FetchRs(instruction);
+			short imm = FetchOffset(instruction);
+			rd.Value = (short)(rs.Value << imm);
+		}
+
+		void Srl(string instruction)
+		{
+			Register rd = FetchRd(instruction);
+			Register rs = FetchRs(instruction);
+			short imm = FetchOffset(instruction);
+			rd.Value = (short)(rs.Value >> imm);
+		}
+
+		void Slti(string instruction)
+		{
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+			short offset = FetchOffset(instruction);
+			rt.Value = (short)(rs.Value < offset ? 1 : 0);
+		}
+		void Beq(string instruction)
+		{
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+			int offset = FetchOffset(instruction);
+
+			if (rs.Value == rt.Value)
+			{
+				CurrentLine += offset;
+			}
+		}
+		void Bne(string instruction)
+		{
+			Register rt = FetchRt(instruction);
+			Register rs = FetchRs(instruction);
+			int offset = FetchOffset(instruction);
+
+			if (rs.Value != rt.Value)
+			{
+				CurrentLine += offset;
+			}
+		}
+		void Muli(string instruction)
+		{
+			Register rs = FetchRs(instruction);
+			Register rt = FetchRt(instruction);
+			short imm = FetchOffset(instruction);
+			rt.Value = (short)(rs.Value * imm);
+		}
+		void Lui(string instruction)
+		{
+
+		}
+		void Lw(string instruction)
+		{
+			Register rt = FetchRt(instruction);
+			short offset = FetchOffset(instruction);
+			rt.Value = dMemory[offset].Value;
+		}
+		void Sw(string instruction)
+		{
+			Register rt = FetchRt(instruction);
+			short offset = FetchOffset(instruction);
+			dMemory[offset].Value = rt.Value;
+		}
+		void J(string instruction)
+		{
+			CurrentLine = FetchJumpOffset(instruction) - 1;
+		}
+		void Jal(string instruction)
+		{
+			Registers[7].Value = (short)(CurrentLine + 1);
+			CurrentLine = FetchJumpOffset(instruction) - 1;
+		}
+
+
+		void Jr(string instruction)
+		{
+			Register rs = FetchRs(instruction);
+			CurrentLine = rs.Value;
+		}
 
 	}
 }
